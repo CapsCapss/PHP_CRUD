eb63798d6e7775f7b3cdfeeff11ce2f699c7ff4a
CapsCapss <87958028+CapsCapss@users.noreply.github.com>
2021-07-26 19:04:10 +0800

    first commit

---
+++ application/controllers/student.php
@@ -0,0 +1,13 @@
+<?php 
+
+    class Student {
+        public function index(){
+            include_once 'views/students.php';
+        }
+
+        public function subjectList(){
+            include_once 'views/subjects.php';
+        }
+    }
+?>
+
---
+++ application/index.php
@@ -0,0 +1,7 @@
+<?php 
+        include_once 'controllers/student.php';
+
+        $student = new Student();
+        $student ->index();
+
+?>
